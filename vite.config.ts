84026cb35213f42f245e65624c88e70439022b0c
fr-informatique <fabian.measson123@gmail.com>
2025-07-10 20:35:05 +0200

    Correction des problèmes de chargement CSS

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
   build: {
     // Optimisations pour le SEO et les performances
     minify: 'terser',
